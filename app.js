bdde9be2bc6ea83c109c2475a387017fc16d14dc
Jai-10 <jai.vaidya18@gmail.com>
2023-04-22 14:28:14 +0530

    updating cors origin - app.js

--- app.js
+++ app.js
@@ -50,8 +50,7 @@ app.use('/deleteDocument', deleteDocRoute)
 
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Origin", "https://certify-0101.netlify.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,  Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
 });
