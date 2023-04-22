a76f6c898a40d2f6dbb9659b0b7004ee6bf21f6f
Jai-10 <jai.vaidya18@gmail.com>
2023-04-22 13:51:41 +0530

    updated cors policy - app.js

--- app.js
+++ app.js
@@ -49,6 +49,12 @@ app.use('/deleteDocument', deleteDocRoute)
 
 
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 // default route
 app.use('/', (req, res) => {
