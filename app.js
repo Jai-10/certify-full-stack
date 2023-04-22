318ba96a3ab0ebd052342887f5c8d2641861cd62
Jai-10 <jai.vaidya18@gmail.com>
2023-04-22 14:12:42 +0530

    updated app.js (cors)

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ app.use('/deleteDocument', deleteDocRoute)
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,  Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
 });
 
