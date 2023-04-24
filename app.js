039134abe632ce81e6ccd67fb7d45443f15c054f
Jai-10 <jai.vaidya18@gmail.com>
2023-04-24 22:55:29 +0530

    minor updates

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const cors = require('cors')
 app.use(cors({
     origin: [
-        "http://localhost:5500/",
+        "http://localhost:5173",
         "https://certify-0101.netlify.app"
     ],
     credentials: true,
